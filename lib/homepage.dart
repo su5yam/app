f33587e5f383d37308331de1be7764783669f9e2
suryam <suryamladdha14@gmail.com>
2021-06-18 20:35:30 +0530

    Day 2

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 30;
+  final String name = 'Suryam';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('bruh $days $name'),
+        ),
+      ),
+      drawer: Drawer(),
+      backgroundColor: Colors.grey,
+    );
+  }
+}

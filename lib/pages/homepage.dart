6e8d8494004f267995c25a10f7c2e8c15bfefa58
suryam <suryamladdha14@gmail.com>
2021-06-19 17:08:31 +0530

    day11

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,10 +2,31 @@ import 'package:app/models/catalog.dart';
 import 'package:app/widgets/drawer.dart';
 import 'package:app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final int days = 30;
+
   final String name = 'Suryam';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {

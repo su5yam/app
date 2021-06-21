789b9b562b918688b679dec0af77793d88e7d625
suryam <suryamladdha14@gmail.com>
2021-06-21 06:19:01 +0530

    day18

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -10,6 +10,57 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
       ),
+      body: Column(
+        children: [
+          _CartListState().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(context.accentColor).make(),
+          SizedBox(
+            width: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(context.theme.buttonColor),
+              shape: MaterialStateProperty.all(StadiumBorder()),
+            ),
+            child: "Buy".text.color(Colors.white).make(),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartListState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
+      ),
     );
   }
 }
